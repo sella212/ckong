7e3069b8542654758bd62e82e3a887ddfcd23932
cukai212 <112556281+cukai212@users.noreply.github.com>
2022-09-01 20:06:34 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "cukong.py",
+    version = "0.1",
+    py_modules = ['cukong'],
+    url = "https://github.com/indraxz/jewer.git",
+    author = "cukai212",
+    install_requires = ["pyngrok"]
+)
